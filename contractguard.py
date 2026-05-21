c557467b8790a57753eb76ea7dcaea0554f5d9cb
djavayours-ui <djava.yours@gmail.com>
2026-05-21 20:48:14 +0500

    Create contractguard.py

---
+++ contractguard.py
@@ -0,0 +1,12 @@
+# meta developer: @djavame
+# scope: hikka_only
+
+from .. import loader
+
+
+@loader.tds
+class ContractGuardMod(loader.Module):
+
+    strings = {
+        "name": "ContractGuard"
+    }
